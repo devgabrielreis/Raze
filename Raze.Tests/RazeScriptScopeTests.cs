51665b5cfc1a78e1ade390eb156fb565e697e400
devgabrielreis <63484148+devgabrielreis@users.noreply.github.com>
2025-05-22 20:53:08 -0300

    adicionando testes para os escopos

---
+++ Raze.Tests/RazeScriptScopeTests.cs
@@ -0,0 +1,28 @@
+﻿using Raze.Script.Core;
+using Raze.Script.Core.Exceptions.RuntimeExceptions;
+using Raze.Script.Core.Scopes;
+
+namespace Raze.Tests;
+
+public class RazeScriptScopeTests
+{
+    [Fact]
+    public void Evaluate_DuplicateVariableDeclaration_ThrowsException()
+    {
+        var scope = new InterpreterScope();
+        RazeScript.Evaluate("var integer x = 1", scope);
+
+        Assert.Throws<RedeclarationException>(() =>
+        {
+            RazeScript.Evaluate("var integer x = 2", scope);
+        });
+
+        var scope2 = new InterpreterScope();
+        RazeScript.Evaluate("var integer x = 1", scope2);
+
+        Assert.Throws<RedeclarationException>(() =>
+        {
+            RazeScript.Evaluate("const integer x = 2", scope2);
+        });
+    }
+}
