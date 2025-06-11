2747ebd6a1e9a66da743efbcab15346e7494fc28
devgabrielreis <63484148+devgabrielreis@users.noreply.github.com>
2025-06-11 20:02:08 -0300

    adicionando testes para o operador de igualdade com booleanos

--- Raze.Tests/RazeScriptBooleanTests.cs
+++ Raze.Tests/RazeScriptBooleanTests.cs
@@ -2,6 +2,7 @@
 using Raze.Script.Core;
 using Raze.Script.Core.Values;
 using Raze.Script.Core.Exceptions.RuntimeExceptions;
+using System.Globalization;
 
 namespace Raze.Tests;
 
@@ -37,6 +38,20 @@ public class RazeScriptBooleanTests
     }
 
     [Theory]
+    [InlineData("true == true", true)]
+    [InlineData("true == false", false)]
+    [InlineData("false == true", false)]
+    [InlineData("false == false", true)]
+    public void Evaluate_BooleanArithmeticExpression_ReturnsExpectedValue(string expression, bool expected)
+    {
+        var scope = new InterpreterScope();
+        var result = RazeScript.Evaluate(expression, scope);
+
+        Assert.IsType<BooleanValue>(result);
+        Assert.Equal(expected, (result as BooleanValue)!.BoolValue);
+    }
+
+    [Theory]
     [InlineData("true + 10")]
     [InlineData("false - 10")]
     [InlineData("true / 10")]
@@ -57,11 +72,34 @@ public class RazeScriptBooleanTests
     [InlineData("false / \"a\"")]
     [InlineData("true * \"a\"")]
     [InlineData("false % \"a\"")]
+    [InlineData("true + true")]
+    [InlineData("false - false")]
+    [InlineData("true / true")]
+    [InlineData("false * false")]
+    [InlineData("true % true")]
     public void Evaluate_InvalidBooleanBinaryOperations_ThrowUnsupportedBinaryOperationException(string expression)
     {
         Assert.Throws<UnsupportedBinaryOperationException>(() =>
         {
             var result = RazeScript.Evaluate(expression);
+        });
+    }
+
+    [Theory]
+    [InlineData("==")]
+    public void Evaluate_BooleanOperationWithNullBooleanVariable_ThrowsNullValueException(string op)
+    {
+        var scope = new InterpreterScope();
+        RazeScript.Evaluate("var boolean nullVar", scope);
+
+        Assert.Throws<NullValueException>(() =>
+        {
+            RazeScript.Evaluate($"false {op} nullVar", scope);
+        });
+
+        Assert.Throws<NullValueException>(() =>
+        {
+            RazeScript.Evaluate($"nullVar {op} true", scope);
         });
     }
 }
