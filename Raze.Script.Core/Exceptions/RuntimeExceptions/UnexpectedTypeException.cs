7ea8456b9d4c799f44427973a27893b90cbfc37c
devgabrielreis <63484148+devgabrielreis@users.noreply.github.com>
2025-06-22 16:27:01 -0300

    adicionando testes para o if-else

---
+++ Raze.Script.Core/Exceptions/RuntimeExceptions/UnexpectedTypeException.cs
@@ -0,0 +1,9 @@
+﻿namespace Raze.Script.Core.Exceptions.RuntimeExceptions;
+
+public class UnexpectedTypeException : RuntimeException
+{
+    public UnexpectedTypeException(string foundType, string expectedType, int? line, int? column)
+        : base($"Expected: {expectedType}. Found: {foundType}", line, column, nameof(UnexpectedTypeException))
+    {
+    }
+}
---
+++ Raze.Tests/RazeScriptControlStructuresTests.cs
@@ -0,0 +1,117 @@
+﻿using Raze.Script.Core;
+using Raze.Script.Core.Exceptions.ParseExceptions;
+using Raze.Script.Core.Exceptions.RuntimeExceptions;
+using Raze.Script.Core.Values;
+
+namespace Raze.Tests;
+
+public class RazeScriptControlStructuresTests
+{
+    [Fact]
+    public void Evaluate_IfWithTrueCondition_ShouldExecute()
+    {
+        var script = @"
+            var integer test = 0;
+            if (10 > 9) {
+                test = 10;
+            }
+            test
+        ";
+
+        var result = RazeScript.Evaluate(script);
+        Assert.IsType<IntegerValue>(result);
+        Assert.Equal(10, (result as IntegerValue)!.IntValue);
+    }
+
+    [Fact]
+    public void Evaluate_IfWithFalseCondition_ShouldntExecute()
+    {
+        var script = @"
+            var integer test = 0;
+            if (9 > 9) {
+                test = 10;
+            }
+            test
+        ";
+
+        var result = RazeScript.Evaluate(script);
+        Assert.IsType<IntegerValue>(result);
+        Assert.Equal(0, (result as IntegerValue)!.IntValue);
+    }
+
+    [Fact]
+    public void Evaluate_IfElseWithFalseCondition_ShouldExecuteElse()
+    {
+        var script = @"
+            var integer test = 0;
+            if (9 > 9) {
+                test = 10;
+            } else {
+                test = 20;
+            }
+            test
+        ";
+
+        var result = RazeScript.Evaluate(script);
+        Assert.IsType<IntegerValue>(result);
+        Assert.Equal(20, (result as IntegerValue)!.IntValue);
+    }
+
+    [Fact]
+    public void Evaluate_IfElse_CanBeChained()
+    {
+        var script = @"
+            var integer test = 0;
+            if (9 > 9) {
+                test = 10;
+            } else if (9 > 9) {
+                test = 20;
+            } else if (9 > 9) {
+                test = 30;
+            } else if (9 > 9) {
+                test = 40;
+            } else if (9 > 0) {
+                test = 50;
+            }
+            test
+        ";
+
+        var result = RazeScript.Evaluate(script);
+        Assert.IsType<IntegerValue>(result);
+        Assert.Equal(50, (result as IntegerValue)!.IntValue);
+    }
+
+    [Fact]
+    public void Evaluate_EmptyIfCondition_ThrowsUnexpectedTokenException()
+    {
+        var script = @"
+            var integer test = 0;
+            if () {
+                test = 10;
+            }
+            test
+        ";
+
+        Assert.Throws<UnexpectedTokenException>(() =>
+        {
+            RazeScript.Evaluate(script);
+        });
+    }
+
+    [Fact]
+    public void Evaluate_NonBooleanIfCondition_ThrowsUnexpectedTypeException()
+    {
+        var script = @"
+            var integer test = 0;
+            if (10 + 10) {
+                test = 10;
+            }
+            test
+        ";
+
+        Assert.Throws<UnexpectedTypeException>(() =>
+        {
+            RazeScript.Evaluate(script);
+        });
+    }
+}
