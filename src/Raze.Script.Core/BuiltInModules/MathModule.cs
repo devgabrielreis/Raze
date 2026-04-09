57737d0b756d8ebd42d3c252aabf5337430a1de7
Gabriel Reis <63484148+devgabrielreis@users.noreply.github.com>
2026-04-09 15:14:07 -0300

    feat: implement remaining math module functions

--- src/Raze.Script.Core/BuiltInModules/MathModule.cs
+++ src/Raze.Script.Core/BuiltInModules/MathModule.cs
@@ -65,6 +65,83 @@ internal static class MathModule
                             .HasType(TypeBuilder.Integer);
                     })
                     .HasBody(IntegerToDecimal);
+            })
+            .HasFunction(functionBuilder =>
+            {
+                functionBuilder
+                    .HasName("ceil")
+                    .HasReturnType(TypeBuilder.Integer)
+                    .HasParameter(parameterBuilder =>
+                    {
+                        parameterBuilder
+                            .HasName("num")
+                            .HasType(TypeBuilder.Decimal);
+                    })
+                    .HasBody(Ceil);
+            })
+            .HasFunction(functionBuilder =>
+            {
+                functionBuilder
+                    .HasName("floor")
+                    .HasReturnType(TypeBuilder.Integer)
+                    .HasParameter(parameterBuilder =>
+                    {
+                        parameterBuilder
+                            .HasName("num")
+                            .HasType(TypeBuilder.Decimal);
+                    })
+                    .HasBody(Floor);
+            })
+            .HasFunction(functionBuilder =>
+            {
+                functionBuilder
+                    .HasName("round")
+                    .HasReturnType(TypeBuilder.Integer)
+                    .HasParameter(parameterBuilder =>
+                    {
+                        parameterBuilder
+                            .HasName("num")
+                            .HasType(TypeBuilder.Decimal);
+                    })
+                    .HasBody(Round);
+            })
+            .HasFunction(functionBuilder =>
+            {
+                functionBuilder
+                    .HasName("maxInteger")
+                    .HasReturnType(TypeBuilder.Integer)
+                    .HasParameter(parameterBuilder =>
+                    {
+                        parameterBuilder
+                            .HasName("num1")
+                            .HasType(TypeBuilder.Integer);
+                    })
+                    .HasParameter(parameterBuilder =>
+                    {
+                        parameterBuilder
+                            .HasName("num2")
+                            .HasType(TypeBuilder.Integer);
+                    })
+                    .HasBody(MaxInteger);
+            })
+            .HasFunction(functionBuilder =>
+            {
+                functionBuilder
+                    .HasName("maxDecimal")
+                    .HasReturnType(TypeBuilder.Decimal)
+                    .HasParameter(parameterBuilder =>
+                    {
+                        parameterBuilder
+                            .HasName("num1")
+                            .HasType(TypeBuilder.Decimal);
+                    })
+                    .HasParameter(parameterBuilder =>
+                    {
+                        parameterBuilder
+                            .HasName("num2")
+                            .HasType(TypeBuilder.Decimal);
+                    })
+                    .HasBody(MaxDecimal);
             });
     }
 
@@ -73,6 +150,53 @@ internal static class MathModule
         var num = (Int128)parameters.Get("num")!;
 
         decimal result = (decimal)num;
+
+        return RazeFunctionReturnValue.FromValue(result);
+    }
+
+    private static RazeFunctionReturnValue Ceil(RazeFunctionParameters parameters)
+    {
+        var num = (decimal)parameters.Get("num")!;
+
+        Int128 result = (Int128)Math.Ceiling(num);
+
+        return RazeFunctionReturnValue.FromValue(result);
+    }
+
+    private static RazeFunctionReturnValue Floor(RazeFunctionParameters parameters)
+    {
+        var num = (decimal)parameters.Get("num")!;
+
+        Int128 result = (Int128)Math.Floor(num);
+
+        return RazeFunctionReturnValue.FromValue(result);
+    }
+
+    private static RazeFunctionReturnValue Round(RazeFunctionParameters parameters)
+    {
+        var num = (decimal)parameters.Get("num")!;
+
+        Int128 result = (Int128)Math.Round(num);
+
+        return RazeFunctionReturnValue.FromValue(result);
+    }
+
+    private static RazeFunctionReturnValue MaxInteger(RazeFunctionParameters parameters)
+    {
+        var num1 = (Int128)parameters.Get("num1")!;
+        var num2 = (Int128)parameters.Get("num2")!;
+
+        Int128 result = (num1 > num2) ? num1 : num2;
+
+        return RazeFunctionReturnValue.FromValue(result);
+    }
+
+    private static RazeFunctionReturnValue MaxDecimal(RazeFunctionParameters parameters)
+    {
+        var num1 = (decimal)parameters.Get("num1")!;
+        var num2 = (decimal)parameters.Get("num2")!;
+
+        decimal result = (num1 > num2) ? num1 : num2;
 
         return RazeFunctionReturnValue.FromValue(result);
     }
