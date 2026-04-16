15b7d6974bcd23af767cd153dca24261ef4ae352
Gabriel Reis <63484148+devgabrielreis@users.noreply.github.com>
2026-04-15 22:35:02 -0300

    feat: add support for command line options

--- src/Raze.Script.Console/Program.cs
+++ src/Raze.Script.Console/Program.cs
@@ -1,11 +1,50 @@
 ﻿using Raze.Script.Console;
+using System.CommandLine;
+using System.Globalization;
 
 internal class Program
 {
     internal static Version Version => typeof(Program).Assembly.GetName().Version!;
 
-    private static void Main(string[] args)
+    internal static int Main(string[] args)
     {
-        RazeConsole.RunInterpreter();
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+        CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
+        var rootCommand = new RootCommand("Raze Script Console - A script runner and REPL for the Raze language");
+
+        var versionOption = new Option<bool>("--version", "-v")
+        {
+            Description = "Show version information"
+        };
+
+        RemoveDefaultVersionOption(rootCommand);
+        rootCommand.Options.Add(versionOption);
+
+        rootCommand.SetAction(parseResult =>
+        {
+            if (parseResult.GetValue(versionOption))
+            {
+                Console.WriteLine(Version.ToString());
+            }
+            else
+            {
+                RazeConsole.RunInterpreter();
+            }
+        });
+
+        return rootCommand.Parse(args).Invoke();
+    }
+
+    private static void RemoveDefaultVersionOption(RootCommand rootCommand)
+    {
+        var helpOption = rootCommand.Options.FirstOrDefault(
+            o => o.Name == "--version"
+        );
+
+        if (helpOption != null)
+        {
+            rootCommand.Options.Remove(helpOption);
+        }
     }
 }
